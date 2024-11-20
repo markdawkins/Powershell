10cc45f0699bd86b9da2acc07778bdd6d976f053
markdawkins <44737733+markdawkins@users.noreply.github.com>
2024-11-20 11:54:45 -0500

    Update Get-ComputerType.ps1

--- Get-ComputerType.ps1
+++ Get-ComputerType.ps1
@@ -1,1 +1,16 @@
+#Powershell script to get computer type
 $computers = Get -ADComputer -Filter *
+
+foreach ($c in $computers) {
+    $os = Get-CimsInstance - ClassName Win32_OperatingSystem - ComputerName $c.Name
+
+
+    switch ($os.ProductType) {
+        1 { $type = 'Workstation' }
+        1 { $type = 'Domain Controller' }
+        1 { $type = 'Server' }
+        Default { $type = 'Unknown' }
+     } 
+      "$($c.Name) is a $type."
+
+}      
